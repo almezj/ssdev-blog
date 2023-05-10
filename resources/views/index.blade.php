3eda4effeaeefc2427f3df6ed93c02beb279a9a5
Josef Žemlička <almezj01@gmail.com>
2023-05-10 10:27:38 +0100

    Changed hero slogan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+				Unleash the power of AI with your coding skills.
                 </h1>
                 <a 
                     href="/blog"
