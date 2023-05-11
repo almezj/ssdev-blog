1df244df98c846cd3fcaa20e305fc2ff6e522663
Reilly00 <114668961+Reilly00@users.noreply.github.com>
2023-05-11 19:52:16 +0100

    Updated index.blade.php with new theme colours
    Updated the entire index.blade.php file with the new colours delcared in the config file.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,47 +2,47 @@
 
 @section('content')
 <div class="background-image grid grid-cols-1 m-auto bg-primary">
-        <div class="flex text-gray-100 pt-10">
+        <div class="flex text-text pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
-                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                <h1 class="sm:text-text text-5xl uppercase font-bold text-shadow-md pb-14">
 				Unleash the power of AI with your coding skills.
                 </h1>
                 <a 
                     href="/blog"
-                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    class="text-center bg-secondary text-text py-2 px-4 font-bold text-xl uppercase">
                     Read More
                 </a>
             </div>
         </div>
     </div>
 
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-text">
         <div>
             <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
         </div>
 
         <div class="m-auto sm:m-auto text-left w-4/5 block">
-            <h2 class="text-3xl font-extrabold text-gray-600">
+            <h2 class="text-3xl font-extrabold text-text">
                 Struggling to be a better web developer?
             </h2>
             
-            <p class="py-8 text-gray-500 text-s">
+            <p class="py-8 text-text text-s">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
             </p>
 
-            <p class="font-extrabold text-gray-600 text-s pb-9">
+            <p class="font-extrabold text-text text-s pb-9">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos molestias porro vero. Vel alias.
             </p>
 
             <a 
                 href="/blog"
-                class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
+                class="uppercase bg-secondary text-text text-s font-extrabold py-3 px-8 rounded-3xl">
                 Find Out More
             </a>
         </div>
     </div>
 
-    <div class="text-center p-15 bg-black text-white">
+    <div class="text-center p-15 bg-secondary text-text">
         <h2 class="text-2xl pb-5 text-l"> 
             I'm an expert in...
         </h2>
@@ -62,33 +62,33 @@
     </div>
 
     <div class="text-center py-15">
-        <span class="uppercase text-s text-gray-400">
+        <span class="uppercase text-s text-text">
             Blog
         </span>
 
-        <h2 class="text-4xl font-bold py-10">
+        <h2 class="text-4xl font-bold py-10 text-text">
             Recent Posts
         </h2>
 
-        <p class="m-auto w-4/5 text-gray-500">
+        <p class="m-auto w-4/5 text-text">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque exercitationem saepe enim veritatis, eos temporibus quaerat facere consectetur qui.
         </p>
     </div>
 
     <div class="sm:grid grid-cols-2 w-4/5 m-auto">
-        <div class="flex bg-yellow-700 text-gray-100 pt-10">
+        <div class="flex bg-secondary text-text pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
-                <span class="uppercase text-xs">
+		<span class="uppercase text-xs text-text">
                     PHP
                 </span>
 
-                <h3 class="text-xl font-bold py-10">
+                <h3 class="text-xl font-bold py-10 text-text">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas necessitatibus dolorum error culpa laboriosam. Enim voluptas earum repudiandae consequuntur ad? Expedita labore aspernatur facilis quasi ex? Nemo hic placeat et?
                 </h3>
 
                 <a 
                     href=""
-                    class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                    class="uppercase bg-transparent border-2 border-text text-text text-xs font-extrabold py-3 px-5 rounded-3xl">
                     Find Out More
                 </a>
             </div>
