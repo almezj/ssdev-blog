44e48e3c30a8ace17d853511d9fb85af6ea54086
Reilly00 <114668961+Reilly00@users.noreply.github.com>
2023-05-11 19:30:20 +0100

    Updated footer.blade.php with new theme colours
    Updated the entire footer.blade.php file with the new colours delcared in the config file.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,28 +1,28 @@
 <footer class="bg-secondary py-20 mt-20">
     <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-text">
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
+            <h3 class="text-l sm:font-bold text-text">
                 Pages
             </h3>
 
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+            <ul class="py-4 sm:text-s pt-4 text-text">
                 <li class="pb-1">
-                    <a href="/">
+                    <a href="/" class="text-text">
                         Home
                     </a>
                 </li>
                 <li class="pb-1">
-                    <a href="/blog">
+                    <a href="/blog" class="text-text">
                         Blog
                     </a>
                 </li>
                 <li class="pb-1">
-                    <a href="/login">
+                    <a href="/login" class="text-text">
                         Login
                     </a>
                 </li>
                 <li class="pb-1">
-                    <a href="/register">
+                    <a href="/register" class="text-text">
                         Register
                     </a>
                 </li>
@@ -30,28 +30,28 @@
         </div>
 
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
+            <h3 class="text-l sm:font-bold text-text">
                 Find Us
             </h3>
 
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+            <ul class="py-4 sm:text-s pt-4 text-text">
                 <li class="pb-1">
-                    <a href="/">
+                    <a href="/" class="text-text">
                         What we do
                     </a>
                 </li>
                 <li class="pb-1">
-                    <a href="/">
+                    <a href="/" class="text-text">
                         Address
                     </a>
                 </li>
                 <li class="pb-1">
-                    <a href="/">
+                    <a href="/" class="text-text">
                         Phone
                     </a>
                 </li>
                 <li class="pb-1">
-                    <a href="/">
+                    <a href="/" class="text-text">
                         Contact
                     </a>
                 </li>
@@ -59,35 +59,35 @@
         </div>
 
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
+            <h3 class="text-l sm:font-bold text-text">
                 Latest posts
             </h3>
 
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+            <ul class="py-4 sm:text-s pt-4 text-text">
                 <li class="pb-1">
-                    <a href="/">
+                    <a href="/" class="text-text">
                         Why we love tech
                     </a>
                 </li>
                 <li class="pb-1">
-                    <a href="/">
+                    <a href="/" class="text-text">
                         Why we love design
                     </a>
                 </li>
                 <li class="pb-1">
-                    <a href="/">
+                    <a href="/" class="text-text">
                         Why to use Laravel
                     </a>
                 </li>
                 <li class="pb-1">
-                    <a href="/">
+                    <a href="/" class="text-text">
                         Why PHP is the best
                     </a>
                 </li>
             </ul>
         </div>
     </div>
-    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-text pt-6">
         Copyright 2017-2021 Code With Dary. All Rights Reserved
     </p>
 </footer>
