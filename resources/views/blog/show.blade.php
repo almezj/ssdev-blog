c03f1d2704bfbeea643e27c8ec3e0d77b5bd91f9
Josef Žemlička <almezj01@gmail.com>
2023-05-13 17:30:27 +0100

    Related posts fixed

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,50 +1,71 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.app') @section('content')
 <div class="w-4/5 m-auto text-left">
     <div class="py-15">
         <h1 class="text-6xl">
             {{ $post->title }}
-			<div class="text-gray-500 text-lg m-2">
-				By <span class="font-bold italic text-gray-800 mb-5">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
-			</div>
+            <div class="text-gray-500 text-lg m-2">
+                By
+                <span
+                    class="font-bold italic text-gray-800 mb-5"
+                    >{{ $post->user->name }}</span
+                >, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+            </div>
         </h1>
-		
-		<div class="mt-5">
-			@foreach ($post->tags as $tag)
-				<span class="post-tag bg-gray-200 rounded-full px-3 py-1 mx-1 text-sm text-gray-700 font-semibold">
-					<a class="pointer-events-none">{{ $tag->name }}</a>
-				</span>
-			@endforeach
-		</div>
-    </div>	
-</div>
 
-<div class="w-4/5 m-auto">
-	<div class="w-1/2">
-        <img src="{{ asset('images/' . $post->image_path) }}" alt="">
+        <div class="mt-5">
+            @foreach ($post->tags as $tag)
+            <span
+                class="post-tag bg-gray-200 rounded-full px-3 py-1 mx-1 text-sm text-gray-700 font-semibold"
+            >
+                <a class="pointer-events-none">{{ $tag->name }}</a>
+            </span>
+            @endforeach
+        </div>
     </div>
-    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-        {{ $post->description }}
-    </p>
-</div>
-<div class="w-4/5 m-auto mt-10">
-	<h2 class="text-3xl font-bold mb-5">Related Posts</h2>
-	<div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-		@foreach ($relatedPosts as $relatedPost)
-			<div class="border border-gray-300 rounded-md p-4">
-				<img src="{{ asset('images/' . $relatedPost->image_path) }}" alt="" class="h-100">
-				<h3 class="text-xl font-bold mb-2">{{ $relatedPost->title }}</h3>
-				<p class="text-gray-600">{{ $relatedPost->description }}</p>
-				<div class="mt-3">
-					@foreach ($relatedPost->tags as $tag)
-						<span class="post-tag bg-gray-200 rounded-full px-3 py-1 mx-1 text-sm text-gray-700 font-semibold">
-							<a class="pointer-events-none">{{ $tag->name }}</a>
-						</span>
-					@endforeach
-				</div>
-			</div>
-		@endforeach
+	<div class="post-body text-2xl">
+		{{ $post->description }}
 	</div>
 </div>
-@endsection 
+
+<div class="w-4/5 m-auto mt-10">
+    <h2 class="text-3xl font-bold mb-5">Related Posts</h2>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        @foreach ($relatedPosts as $relatedPost)
+        <a
+            href="{{ route('blog.show', $relatedPost->slug) }}"
+            class="no-underline"
+        >
+            <div class="rounded overflow-hidden shadow-lg flex-column">
+                <div class="aspect-w-16 aspect-h-9">
+                    <img
+                        src="{{ asset('images/' . $relatedPost->image_path) }}"
+                        alt=""
+                        class="object-cover post-img"
+                    />
+                </div>
+                <div class="flex-1">
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">
+                            {{ $relatedPost->title }}
+                        </div>
+                        <p class="text-gray-700 text-base">
+                            {{ $relatedPost->description }}
+                        </p>
+                    </div>
+                    <div class="px-6 pt-4 pb-2">
+                        @foreach ($relatedPost->tags as $tag)
+                        <span
+                            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                        >
+                            {{ $tag->name }}
+                        </span>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </a>
+        @endforeach
+    </div>
+</div>
+
+@endsection
